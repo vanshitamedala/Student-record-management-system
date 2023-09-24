54145a543800d5c6b76961d59c1c2470a5386ce2
Vanshita <118332400+vanshitamedala@users.noreply.github.com>
2023-09-24 20:18:01 +0530

    Update StudentRecordApp.java

--- StudentRecordApp.java
+++ StudentRecordApp.java
@@ -1,72 +1,152 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.List;
 
-public class StudentRecordApp {
-    private List<Student> studentList = new ArrayList<>();
-    private JFrame frame;
-    private DefaultListModel<String> listModel;
-    private JList<String> studentListView;
+class Student {
+    private String name;
+    private String dateOfBirth;
+    private String contactDetails;
+    private String enrollmentDate;
 
-    public StudentRecordApp() {
-        frame = new JFrame("Student Record Management");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-
-        listModel = new DefaultListModel<>();
-        studentListView = new JList<>(listModel);
-
-        JScrollPane scrollPane = new JScrollPane(studentListView);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-
-        JTextField nameField = new JTextField(15);
-        JButton addButton = new JButton("Add Student");
-
-        addButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String studentName = nameField.getText();
-                if (!studentName.isEmpty()) {
-                    Student student = new Student(studentName);
-                    studentList.add(student);
-                    listModel.addElement(student.getName());
-                    nameField.setText("");
-                }
-            }
-        });
-
-        panel.add(nameField);
-        panel.add(addButton);
-
-        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-        frame.getContentPane().add(panel, BorderLayout.SOUTH);
-
-        frame.setVisible(true);
+    public Student(String name, String dateOfBirth, String contactDetails, String enrollmentDate) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.contactDetails = contactDetails;
+        this.enrollmentDate = enrollmentDate;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(String contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public String getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(String enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nDate of Birth: " + dateOfBirth + "\nContact Details: " + contactDetails + "\nEnrollment Date: " + enrollmentDate;
+    }
+}
+
+public class StudentRecord {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new StudentRecordApp();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Student Record Management System");
+            System.out.println("1. Add Student");
+            System.out.println("2. Update Student Information");
+            System.out.println("3. View Student Records");
+            System.out.println("4. Search Students");
+            System.out.println("5. Generate Reports");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Adding a new student:");
+                    System.out.print("Enter Name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter Date of Birth: ");
+                    String dateOfBirth = scanner.nextLine();
+                    System.out.print("Enter Contact Details: ");
+                    String contactDetails = scanner.nextLine();
+                    System.out.print("Enter Enrollment Date: ");
+                    String enrollmentDate = scanner.nextLine();
+
+                    Student newStudent = new Student(name, dateOfBirth, contactDetails, enrollmentDate);
+                    students.add(newStudent);
+                    System.out.println("Student added successfully.");
+                    break;
+
+                case 2:
+                    System.out.println("Updating student information:");
+                    System.out.print("Enter the name of the student to update: ");
+                    String studentName = scanner.nextLine();
+
+                    for (Student student : students) {
+                        if (student.getName().equals(studentName)) {
+                            System.out.print("Enter new Name: ");
+                            student.setName(scanner.nextLine());
+                            System.out.print("Enter new Date of Birth: ");
+                            student.setDateOfBirth(scanner.nextLine());
+                            System.out.print("Enter new Contact Details: ");
+                            student.setContactDetails(scanner.nextLine());
+                            System.out.print("Enter new Enrollment Date: ");
+                            student.setEnrollmentDate(scanner.nextLine());
+                            System.out.println("Student information updated successfully.");
+                            break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Viewing student records:");
+                    for (Student student : students) {
+                        System.out.println(student);
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Searching for students:");
+                    System.out.print("Enter student name to search: ");
+                    String searchName = scanner.nextLine();
+                    boolean found = false;
+
+                    for (Student student : students) {
+                        if (student.getName().equals(searchName)) {
+                            System.out.println("Student Found:\n" + student);
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Student not found.");
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Generating reports:");
+                    // Implement code to generate reports here
+                    break;
+
+                case 6:
+                    System.out.println("Exiting the program. Goodbye!");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
             }
-        });
-    }
-
-    class Student {
-        private String name;
-
-        public Student(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
         }
     }
 }
